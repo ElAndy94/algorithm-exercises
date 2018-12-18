68fde2e333c6f3ad52258ebf29baeaf3d2dc18ef
Andrew <andrewpeliza@hotmail.com>
2018-12-18 15:09:30 +0000

    Two palindrome solutions, one short and one long solution.

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -1,3 +1,22 @@
-function palindrome(str) {}
+function palindrome(str) {
+  const reversed = str.split('').reverse().join('');
+
+  return str === reversed;
+};
 
 module.exports = palindrome;
+
+
+// let original = str;
+//     let reversed = '';
+
+//     for (let char of str) {
+//         reversed = char + reversed
+//     }
+
+//     if (original === reversed) {
+//         console.log(`${str} is a palindrome`);
+//         return true;
+//     } else {
+//         return false;
+//     }
