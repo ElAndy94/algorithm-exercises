477356377d56e0dbbc1318ece6832e422b9b0370
Andrew <andrewpeliza@hotmail.com>
2018-12-18 14:56:57 +0000

    4 Different String reversal algorithms.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,3 +1,30 @@
-function reverse(str) {}
+reverse = (str) => {
+    return str.split('').reduce((rev, char) => char + rev, '');
+
+    // return str.split('').reduce((reversed, character) => {
+    //     return character + reversed;
+    // }, '');
+}
 
 module.exports = reverse;
+
+// let reversed = '';
+
+//     for (let character of str) {
+//         reversed = character + reversed;
+//     }
+
+//     return reversed;
+
+
+////////////
+
+// return str.split('').reverse().join('');
+
+/////////
+
+    // strSplit = str.split('');
+    // reversed = strSplit.reverse();
+    // finished = reversed.join('');
+
+    // return finished
