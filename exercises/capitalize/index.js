35c8e11650fef6ef80efbd8d469a9f8c2875db67
Andrew <andrewpeliza@hotmail.com>
2018-12-19 14:33:13 +0000

    Capitalise and Steps complete with advanced concepts.

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -1,3 +1,23 @@
-function capitalize(str) {}
+capitalize = (str) => {
+    words = [];
+
+    for (let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1))
+    }
+
+    return words.join(' ');
+};
 
 module.exports = capitalize;
+
+// result = str[0].toUpperCase();
+
+    // for (let i = 1; i < str.length; i++) {
+    //     if (str[i - 1] === ' ') {
+    //         result += str[i].toUpperCase();
+    //     } else {
+    //         result += str[i];
+    //     }
+    // }
+
+    // return result;
