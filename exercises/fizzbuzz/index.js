f76430011035e1f09deef6330651b2c1057d12bf
Andrew <andrewpeliza@hotmail.com>
2018-12-20 15:20:34 +0000

    Vowel counter complete.

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -1,4 +1,4 @@
-function fizzBuzz(n) {
+fizzBuzz = (n) => {
     for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
             console.log('fizzbuzz');
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -1,3 +1,21 @@
-function vowels(str) {}
+vowels = (str) => {
+   const matches = str.match(/[aeiou]/gi);
+
+   return matches ? matches.length : 0;
+};
 
 module.exports = vowels;
+
+// vowels = (str) => {
+//     let count = 0;
+//     // const checker = "aeiou";  OR THIS
+//     const checker = ['a', 'e', 'i', 'o', 'u']; // way better if you want to include things like 'abc';
+
+//     for (let char of str.toLowerCase()) {
+//         if (checker.includes(char)) {
+//             count ++;
+//         }
+//     }
+
+//     return count;
+// };
