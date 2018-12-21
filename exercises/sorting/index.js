5068d83111e74d3e62db9329f7581b565f2821c1
Andrew <andrewpeliza@hotmail.com>
2018-12-21 14:16:24 +0000

    bubbleSort complete.

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -1,16 +1,25 @@
-function bubbleSort(arr) {
+bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < (arr.length - i - 1); j++) {
+            if (arr[j] > arr[j+1]) {
+                const temp = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+selectionSort = (arr) => {
 
 }
 
-function selectionSort(arr) {
+mergeSort = (arr) => {
 
 }
 
-function mergeSort(arr) {
-
-}
-
-function merge(left, right) {
+merge = (left, right) => {
 
 }
 
