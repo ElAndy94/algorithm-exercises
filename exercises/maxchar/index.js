c6895171acfba194cfe7823945cd9e64a30b9d07
Andrew <andrewpeliza@hotmail.com>
2018-12-27 21:58:21 +0100

    Max Char second attempt

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -3,9 +3,6 @@ maxChar = (str) => {
     let max = 0;
     let maxChar = '';
 
-    // for (let char of str) {
-    //     chars[char] = chars[char] + 1 || 1;
-    // }
     for (let char of str) {
         if (!chars[char]) {
             chars[char] = 1;
@@ -24,3 +21,26 @@ maxChar = (str) => {
 };
 
 module.exports = maxChar;
+
+// const chars = {};
+// let max = 0;
+// let maxChar = '';
+
+// // for (let char of str) {
+// //     chars[char] = chars[char] + 1 || 1;
+// // }
+// for (let char of str) {
+//     if (!chars[char]) {
+//         chars[char] = 1;
+//     } else {
+//         chars[char]++;
+//     }
+// }
+
+// for (let char in chars) {
+//     if (chars[char] > max) {
+//         max = chars[char];
+//         maxChar = char;
+//     }
+// }
+// return maxChar;
